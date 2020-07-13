bf6393e4b858b90942be5fa01236102c4079ffb5
Pauline <pauline.eustachy@gmail.com>
2020-07-14 01:13:41 +0200

    made portfolio responsive

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
-// This is where it all goes :)
+document.addEventListener('DOMContentLoaded', () => {
+  calculateAge();
+});
+
+const calculateAge = () => {
+  let span = document.querySelector('#age');
+  let birthYear = 1993;
+  let actualYear = new Date().getFullYear();
+  let age = actualYear - birthYear;
+
+  span.innerText = age;
+};
