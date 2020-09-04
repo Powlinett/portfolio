61f9fc6000c0305db6753664a8aa56cf5fe89b8d
Pauline <pauline.eustachy@gmail.com>
2020-09-04 05:48:00 +0200

    Automated commit at 2020-09-04 03:48:00 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,48 @@
-// This is where it all goes :)
+document.addEventListener('DOMContentLoaded', () => {
+  calculateAge();
+  copyContact();
+  resetContactContent();
+});
+
+const calculateAge = () => {
+  let span = document.querySelector('#age');
+  let birthYear = 1993;
+  let actualYear = new Date().getFullYear();
+  let age = actualYear - birthYear;
+
+  span.innerText = age;
+};
+
+const copyContact = () => {
+  const contacts = document.querySelectorAll('.media-link .content');
+
+  contacts.forEach((contact) => {
+    contact.addEventListener('click', (e) => {
+      let input = document.createElement("input");
+      input.name = 'clipboard';
+      input.style.opacity = "0";
+      document.body.appendChild(input);
+
+      input.type = "text";
+      input.value = e.target.innerText;
+      input.select();
+      document.execCommand("copy");
+
+      let copiedInput = document.querySelector("input[name='clipboard']");
+      copiedInput.remove();
+      e.target.setAttribute('data-after', 'Copié');
+    });
+  });
+};
+
+const resetContactContent = () => {
+  const contacts = document.querySelectorAll('.media-link .content');
+
+  contacts.forEach((contact) => {
+    contact.addEventListener('mouseout', (e) => {
+      if (e.target.getAttribute('data-after') == 'Copié') {
+        e.target.setAttribute('data-after', 'Cliquer pour copier');
+      }
+    });
+  });
+};
